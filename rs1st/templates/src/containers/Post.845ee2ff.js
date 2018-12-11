37d28b28d4ec8c8319af58a8d50e0593b7a2af47
kito <hoge@hoge.com>
2018-12-11 17:54:27 +0900

    Update.

---
+++ rs1st/templates/src/containers/Post.845ee2ff.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{176:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=l(1),n=function(e){return e&&e.__esModule?e:{default:e}}(u),a=l(116);t.default=(0,a.withRouteData)(function(e){var t=e.post;return n.default.createElement("div",null,n.default.createElement(a.Link,{to:"/"},"<"," Back"),n.default.createElement("br",null),n.default.createElement("h3",null,t.title),n.default.createElement("p",null,t.body))})}});
+//# sourceMappingURL=Post.845ee2ff.js.map
